ecfa437092d0cdf381967afeadd1674c96603970
finbarr12 <finarrogbonna@gmail.com>
2023-06-03 00:49:52 +0100

    added

--- dist/Controller/AdminController.js
+++ dist/Controller/AdminController.js
@@ -1,132 +1,174 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResetPasssword = exports.ForgotPassword = exports.SignInadmin = exports.SignUpAdmin = void 0;
+exports.ResetPasssword =
+  exports.ForgotPassword =
+  exports.SignInadmin =
+  exports.SignUpAdmin =
+    void 0;
 const AdminModel_1 = __importDefault(require("../Model/AdminModel"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const nodemailer_1 = __importDefault(require("nodemailer"));
-const nodemailer_sendinblue_transport_1 = __importDefault(require("nodemailer-sendinblue-transport"));
+const nodemailer_sendinblue_transport_1 = __importDefault(
+  require("sendinblue-api")
+);
 const envVariables_1 = __importDefault(require("../Environment/envVariables"));
 //signupadmin
-const SignUpAdmin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const SignUpAdmin = (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { name, password, email, confirmpassword } = req.body;
-        const salt = yield bcrypt_1.default.genSalt(12);
-        const hashedPass = yield bcrypt_1.default.hash(password, salt);
-        const SignAdmin = yield AdminModel_1.default.create({
-            name,
-            password: hashedPass,
-            email,
-            confirmpassword: hashedPass,
-        });
-        return res.status(201).json({
-            message: "done",
-            data: SignAdmin,
-        });
+      const { name, password, email, confirmpassword } = req.body;
+      const salt = yield bcrypt_1.default.genSalt(12);
+      const hashedPass = yield bcrypt_1.default.hash(password, salt);
+      const SignAdmin = yield AdminModel_1.default.create({
+        name,
+        password: hashedPass,
+        email,
+        confirmpassword: hashedPass,
+      });
+      return res.status(201).json({
+        message: "done",
+        data: SignAdmin,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: "An error occured",
+        data: error,
+      });
     }
-    catch (error) {
-        return res.status(400).json({
-            message: "An error occured",
-            data: error,
-        });
-    }
-});
+  });
 exports.SignUpAdmin = SignUpAdmin;
 //signinAdmin
-const SignInadmin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const SignInadmin = (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { email, password } = req.body;
-        if (!email) {
-            res.status(400).json({
-                message: "Input email",
-            });
-        }
-        const adminInfo = yield AdminModel_1.default.findOne({ email });
-        if (!adminInfo) {
-            res.status(400).json({
-                message: "Email not found",
-            });
-        }
-        const Checkpass = yield bcrypt_1.default.compare(password, adminInfo.password);
-        if (!Checkpass) {
-            return res.status(404).json({
-                message: "User dosen't exist",
-            });
-        }
-        return res.status(200).json({
-            status: "success",
-            data: adminInfo,
+      const { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({
+          message: "Input email",
         });
-    }
-    catch (error) {
-        return res.status(400).json({
-            message: "An error occured",
-            data: error,
+      }
+      const adminInfo = yield AdminModel_1.default.findOne({ email });
+      if (!adminInfo) {
+        res.status(400).json({
+          message: "Email not found",
         });
+      }
+      const Checkpass = yield bcrypt_1.default.compare(
+        password,
+        adminInfo.password
+      );
+      if (!Checkpass) {
+        return res.status(404).json({
+          message: "User dosen't exist",
+        });
+      }
+      return res.status(200).json({
+        status: "success",
+        data: adminInfo,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: "An error occured",
+        data: error,
+      });
     }
-});
+  });
 exports.SignInadmin = SignInadmin;
-const ForgotPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const ForgotPassword = (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { email } = req.body;
-        const user = yield AdminModel_1.default.findOne({ email });
-        if (!user) {
-            return res.status(404).send({ message: "User not found" });
-        }
-        const pin = Math.floor(Math.random() * 900000) + 100000;
-        user.passwordResetPin = pin;
-        yield user.save();
-        const transport = nodemailer_1.default.createTransport(new nodemailer_sendinblue_transport_1.default({
-            apiKey: envVariables_1.default.send_in_blue,
-        }));
-        const sendEmail = {
-            from: "stsixtus12@gmail.com",
-            to: user.email,
-            subject: "Password reset PIN",
-            html: `Your password reset PIN is: <h3>${pin}</h3>`,
-        };
-        transport.sendMail(sendEmail);
-        res.status(200).json({
-            messge: `PIN sent to ${user === null || user === void 0 ? void 0 : user.email}`,
-        });
+      const { email } = req.body;
+      const user = yield AdminModel_1.default.findOne({ email });
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      const pin = Math.floor(Math.random() * 900000) + 100000;
+      user.passwordResetPin = pin;
+      yield user.save();
+      const transport = nodemailer_1.default.createTransport(
+        new nodemailer_sendinblue_transport_1.default({
+          apiKey: envVariables_1.default.send_in_blue,
+        })
+      );
+      const sendEmail = {
+        from: "stsixtus12@gmail.com",
+        to: user.email,
+        subject: "Password reset PIN",
+        html: `Your password reset PIN is: <h3>${pin}</h3>`,
+      };
+      transport.sendMail(sendEmail);
+      res.status(200).json({
+        messge: `PIN sent to ${
+          user === null || user === void 0 ? void 0 : user.email
+        }`,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: "An error occured",
+        data: error,
+      });
     }
-    catch (error) {
-        return res.status(400).json({
-            message: "An error occured",
-            data: error,
-        });
-    }
-});
+  });
 exports.ForgotPassword = ForgotPassword;
-const ResetPasssword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const ResetPasssword = (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { email, pin, newPassword } = req.body;
-        const user = yield AdminModel_1.default.findOne({ email, passwordResetPin: pin });
-        if (!user) {
-            return res.status(404).send({ message: "Invalid email or PIN" });
-        }
-        user.password = newPassword;
-        user.confirmpassword = newPassword;
-        user.passwordResetPin = undefined;
-        yield user.save();
-        res.send({ message: "Password reset successful" });
+      const { email, pin, newPassword } = req.body;
+      const user = yield AdminModel_1.default.findOne({
+        email,
+        passwordResetPin: pin,
+      });
+      if (!user) {
+        return res.status(404).send({ message: "Invalid email or PIN" });
+      }
+      user.password = newPassword;
+      user.confirmpassword = newPassword;
+      user.passwordResetPin = undefined;
+      yield user.save();
+      res.send({ message: "Password reset successful" });
+    } catch (error) {
+      return res.status(400).json({
+        message: "An error occured",
+        data: error,
+      });
     }
-    catch (error) {
-        return res.status(400).json({
-            message: "An error occured",
-            data: error,
-        });
-    }
-});
+  });
 exports.ResetPasssword = ResetPasssword;
--- dist/router/router.js
+++ dist/router/router.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const AdminController_1 = require("../Controller/AdminController");
@@ -11,10 +13,14 @@ const Mailer_1 = require("../Controller/Mailer");
 const router = (0, express_1.Router)();
 router.post("/adminsignup", AdminController_1.SignUpAdmin);
 router.post("/adminsignin", AdminController_1.SignInadmin);
-router.post("/createproduct", Multer_1.default, ProductController_1.CreateProducts);
+router.post(
+  "/createproduct",
+  Multer_1.default,
+  ProductController_1.CreateProducts
+);
 router.get("/products", ProductController_1.Allproducts);
 router.post("/mailer", Mailer_1.Mailer);
 router.delete("/delete/:id", ProductController_1.DeleteProduct);
-router.post("/forgotpass", AdminController_1.ForgotPassword);
-router.post("/resetpass", AdminController_1.ResetPasssword);
+// router.post("/forgotpass", AdminController_1.ForgotPassword);
+// router.post("/resetpass", AdminController_1.ResetPasssword);
 exports.default = router;
